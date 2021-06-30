9e3a39f7229a72310e810c2ba02bfcf1f6793b88
daniel48kassem <k1999daniel@gmail.com>
2021-06-30 18:41:22 +0300

    init

--- app/Http/Controllers/ImageUploadController.php
+++ app/Http/Controllers/ImageUploadController.php
@@ -6,5 +6,21 @@ use Illuminate\Http\Request;
 
 class ImageUploadController extends Controller
 {
-    //
+    public function upload(Request $request){
+
+        //
+        //upload to original folder
+        //and every time the image requested at specific resolution the jobs dispatched
+
+        $this->validate($request,[
+            'image'=>['required','mime:jpeg,png','max:2048']
+        ]);
+
+        $image=$request->file('image');
+        $image_path=$image->getPathname();
+        $image_name=time().preg_replace('/\s+/','_',strtolower($image->getClientOriginalName()));
+
+        $tmp=$image->storeAs('uploads/original',$image_name);
+
+    }
 }
